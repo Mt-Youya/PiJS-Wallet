2405403d4c0c32792fa4795fdc24dcdd0b399a76
Mt-Youya <dd257248@163.com>
2025-02-19 20:23:33 +0800

    fix: loading bugfix

--- src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-function Loading() {
-    return (
-            <img className="block mx-auto animate-spin my-2"  src="/assets/Loading.svg" alt="loading..."/>
-    )
-}
-
-export default Loading
--- src/pages/Home/comopnents/textinput.jsx
+++ src/pages/Home/comopnents/textinput.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
+import { Loader } from "lucide-react"
 import { toast } from "sonner"
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "@/ui/dialog.jsx"
 import { Button } from "@/ui/button.jsx"
@@ -11,7 +12,6 @@ import { userinfoStore } from "@/stores/userinfo.js"
 import { USDTAbi, Abi } from "@/constants/contract.json"
 import { BrowserProvider, Contract } from "ethers"
 import { Session } from "@/utils/storage.js"
-import Loading from "@/components/Loading.jsx"
 
 function TextInput() {
     const { userinfo, setUserinfo } = userinfoStore()
@@ -26,14 +26,14 @@ function TextInput() {
     async function handlePay() {
         if (!userinfo) {
             setUserinfo(Session.get("userInfo"))
-            setOpen(false)
+            // setOpen(false)
             return toast.warning("请先连接钱包!")
         }
         setLoading(true)
         const { data: payInfo } = await paymentInfo()
 
         if (!payInfo) {
-            setOpen(false)
+            // setOpen(false)
             return toast.error("支付信息查询失败！")
         }
         const { hash: txHash } = await SendTx(payInfo)
@@ -97,40 +97,38 @@ function TextInput() {
                 <Textarea
                     className="border data-[hover]:shadow data-[focus]:bg-blue-100 block w-full h-32 bg-[#2A2A2A] rounded-lg" />
             </div>
-            {
-                !isPaid ?
-                    <Dialog as="div" className="relative z-10 focus:outline-none" open={open}>
-                        <DialogTrigger
-                            className="bg-primary p-3 rounded-xl w-[calc(100%-2rem)] m-auto block"
-                            onClick={handlePay}
-                        >
-                            {t("支付100USDT参与活动")}
-                        </DialogTrigger>
-                        <DialogContent
-                            className="max-w-md text-center text-white border-none w-7/12 rounded-xl bg-white/5 p-6 backdrop-blur-2xl "
-                        >
-                            {loading && !approval ?
-                                <>  <Loading /> <span>{t("批准100USDT参与活动")}</span>  </>
-                                : <> <Loading /> <span>{t("支付100USDT参与活动")}</span> </>
-                            }
-                            {isPaid && (
-                                <div className="mt-4">
-                                    <p>{t("支付完成")}。{t("您已完成私募")}</p>
-                                    <br />
-                                    <DialogClose
-                                        className="w-30 h-12 bg-primary rounded-md text-black"> 知道了 </DialogClose>
-                                </div>
-                            )}
-                            <DialogTitle> <DialogDescription /> </DialogTitle>
-                        </DialogContent>
-                    </Dialog>
-                    : (
-                        <Button className="bg-[#5D6167] p-3 rounded-xl w-[calc(100%-2rem)] m-auto block" disabled>
-                            {t("您已完成私募")}
-                        </Button>
-                    )
+            {isPaid ?
+                <Dialog as="div" className="relative z-10 focus:outline-none">
+                    <DialogTrigger
+                        className="bg-primary p-3 rounded-xl w-[calc(100%-2rem)] m-auto block"
+                        onClick={handlePay}
+                    >
+                        {t("支付100USDT参与活动")}
+                    </DialogTrigger>
+                    <DialogContent
+                        className="max-w-md text-center text-white border-none w-7/12 rounded-xl bg-white/5 p-6 backdrop-blur-2xl justify-center items-center"
+                    >
+                        {loading && (
+                            <>
+                                {!approval && <Loader className="animate-spin scale-150 text-center block mx-auto my-4" />}
+                                <span>{t(`${!approval ? "批准" : "支付"}100USDT参与活动`)}</span>
+                            </>
+                        )}
+                        {!isPaid && (
+                            <div className="mt-4">
+                                <p>{t("支付完成")}。{t("您已完成私募")}</p>
+                                <br />
+                                <DialogClose className="w-30 h-12 bg-primary rounded-md text-black"> 知道了 </DialogClose>
+                            </div>
+                        )}
+                        <DialogTitle> <DialogDescription /> </DialogTitle>
+                    </DialogContent>
+                </Dialog>
+                :
+                <Button className="bg-[#5D6167] p-3 rounded-xl w-[calc(100%-2rem)] m-auto block" disabled>
+                    {t("您已完成私募")}
+                </Button>
             }
-
         </>
     )
 }
