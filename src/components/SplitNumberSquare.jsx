969eeca736df8fab0bc6f8d207804f3db2849673
Mt-Youya <dd257248@163.com>
2025-02-19 17:49:44 +0800

    fix: invite code url

--- src/components/SplitNumberSquare.jsx
+++ src/components/SplitNumberSquare.jsx
@@ -5,7 +5,7 @@ function SplitNumberSquare({ number = 0, size = "large" }) {
     return (
         <ul className={clsx(`w-fit ml-auto grid grid-cols-${len} `, size === "large" ? `*:p-3 gap-2` : `*:p-1 gap-1`)}
             style={{ gridTemplateColumns: `repeat(${len}, 1fr)` }}>
-            {number.toString().split("").map((num, index) => (
+            {number?.toString().split("").map((num, index) => (
                 <li
                     className={clsx(`text-center p-1 bg-[#1F2328] border-solid-grey`, size === "small" ? "w-8 h-8" : "w-12 h-12")}
                     key={index}
